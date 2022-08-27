18e2f112c5248bebe1f779cb295239623fe0428a
noernova <noernova666@gmail.com>
2022-08-28 02:25:41 +0700

    add tts function

---
+++ text-to-speech.js
@@ -0,0 +1,33 @@
+/* jshint esversion: 8 */
+
+const SHN_TTS_API = "https://shantts.herokuapp.com/api/?text=";
+
+// speak function
+function browserSpeak(msg, vox) {
+  const sp = new SpeechSynthesisUtterance();
+  sp.voice = window.speechSynthesis.getVoices().filter((e) => {
+    return vox == e.name;
+  })[0];
+  sp.text = msg;
+  speechSynthesis.speak(sp);
+}
+
+async function shanTTSSpeak(msg) {
+  try {
+    const e = await fetch(SHN_TTS_API + msg);
+    const { data } = await e.json();
+
+    if (!data) return;
+
+    const audio = new Audio(`data:audio/wav;base64,${data}`);
+    audio.play();
+  } catch (err) {}
+}
+
+function speakMe(word, endpoint) {
+  if (endpoint === "eng2shn") {
+    browserSpeak(word, "Google US English");
+    return;
+  }
+  shanTTSSpeak(word);
+}
